307d6900fe29c0b272d3a6020cdf94428a11ac15
LeoLizc <leolizcanop@gmail.com>
2024-05-02 00:56:04 -0500

    Add unit tests for SMSwitcher class

---
+++ tests/switcher.test.ts
@@ -0,0 +1,14 @@
+describe('SMSwitcher', () => {
+  it('should be defined', () => {});
+});
+
+/*
+  - Encadenamiento
+  - AutoBreake
+  - Callback case
+  - Value case
+  - Callback handler
+  - Value handler
+  - Defaults
+  - Execution
+ */
